53be56fde5c5b746704148405309a0ba9266fbd2
achref124 <ouerfelliiachref@gmail.com>
2023-02-10 16:14:14 +0100

    first commit

---
+++ src/Component/Profile/Adress.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Adress = () => {
+  return (
+    <div>
+        <h2>
+            Rue Nassim-Sers-Kef
+        </h2>
+    </div>
+  )
+}
+
+export default Adress
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProfilePhoto = () => {
+  return (
+    <div >
+        <img style={{width:'250px',borderRadius:'50%'}} src='https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/217730792_4409824815727110_7057865548945378740_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=IZNraOh7OwkAX-gODLW&_nc_ht=scontent.ftun9-1.fna&oh=00_AfAEEWBGDnKNRC0qFwY0xLa7N2ygu1oUMHzaN6t99fdcNw&oe=63EAB6F0' alt='profile pic'></img>
+    </div>
+  )
+};
+
+export default ProfilePhoto
